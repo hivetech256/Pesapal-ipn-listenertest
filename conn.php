e62b3d5f805faa1d80528d6269a1f95873fde239
hivetech256 <njukijoseph256@gmail.com>
2025-04-24 17:32:49 -0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,1 +1,15 @@
+<?php
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+$dburi = getenv('DB_URI');
+$dbport = getenv('DB_PORT');
 
+$conn = new mysqli($servername, $username, $password, $dbname, $dburi, $dbport);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
