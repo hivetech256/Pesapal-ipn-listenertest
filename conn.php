9432eaac7cf06164c85b97050342a92bc114d8e9
hivetech256 <njukijoseph256@gmail.com>
2025-04-24 18:35:45 -0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,10 +3,9 @@ $servername = getenv('DB_HOST');
 $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
-$dburi = getenv('DB_URI');
-$dbport = getenv('DB_PORT');
+$dbport = getenv('DB_PORT'); // This should be the port number (e.g., 3306)
 
-$conn = new mysqli($servername, $username, $password, $dbname, $dburi, $dbport);
+$conn = new mysqli($servername, $username, $password, $dbname, $dbport); // Removed $dburi from the parameters
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
